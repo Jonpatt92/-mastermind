95347d6941950a74ec6b54903ed8b2d2d889d2a0
Jon Patterson <JonPatt92@gmail.com>
2019-10-18 10:55:57 -0600

    Fixed initialize for sequence

--- lib/sequence.rb
+++ lib/sequence.rb
@@ -4,7 +4,7 @@ class Sequence
   def initialize(colors = nil)
     @color_seed = ["R", "R", "G", "G", "B", "B", "Y", "Y"]
     @colors = colors
-    @colors = @color_seed.shuffle[0..3] if colors == nil 
+    @colors = @color_seed.shuffle[0..3] if colors == nil
   end
 
   def shuffle_colors
@@ -29,6 +29,6 @@ class Sequence
       end
     end
 
-    puts "'#{guess.join}' has #{correct_elements} of the correct elements with #{correct_positions} in the correct positions"
+    print "'#{guess.join}' has #{correct_elements} of the correct elements with #{correct_positions} in the correct positions"
   end
 end
