e0f2357b0ad4eb6a88778850fdc7728db62d76f8
Jon Patterson <JonPatt92@gmail.com>
2019-10-18 10:51:28 -0600

    Add evaluate guess method

--- lib/sequence.rb
+++ lib/sequence.rb
@@ -16,6 +16,18 @@ class Sequence
   end
 
   def evaluate_guess(guess)
-  
+    correct_positions = 0
+    correct_elements_array = guess.find_all {|guess_color| @colors.any?(guess_color)}
+    correct_elements = correct_elements_array.length
+
+    @colors.each_with_index do |color, c_index|
+      guess.each_with_index do |g_color, g_index|
+        if color == g_color && c_index == g_index
+          correct_positions +=1
+        end
+      end
+    end
+
+    puts "'#{guess.join}' has #{correct_elements} of the correct elements with #{correct_positions} in the correct positions"
   end
 end
