e04fa527fe2629b437fff8ed18f2a7e0e73e4223
Jon Patterson <JonPatt92@gmail.com>
2019-10-18 11:10:39 -0600

    midway through building game class

--- lib/game.rb
+++ lib/game.rb
@@ -5,7 +5,45 @@ class Game
     @guess = nil
   end
 
-  def play
-    @guess = gets.chomp
+  def start_game
+    quit = nil
+    until quit == "q"
+      p "Welcome to MASTERMIND"
+      p "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
+      p ">"
+      valid_input = false
+      until valid_input
+        initial_input = gets.chomp
+        if initial_input.downcase == "p"
+          valid_input = true
+          initialize()
+          play_game()
+        elsif initial_input.downcase == "q"
+          valid_input = true
+          quit = "q"
+          puts "Thanks for playing!"
+        elsif initial_input.downcase == "i"
+          p "I will select 4 random letters. Either (R)ed, (G)reen, (Y)ellow or (B)lue"
+          p "Each letter can appear no more than 2 times, the total letters will always be 4"
+          p "You must guess the 4 letters in order"
+          p "ex: RGYB"
+          p "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
+        else p "That was not a valid input. Enter 'p' to play. Enter 'q' to quit."
+        end
+      end
+    end
+  end
+
+  def valid_guess?
+
+  end
+
+  def play_game
+    p "I have generated a beginner sequence with four elements made up of: (r)ed,
+    (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game.
+    What's your guess?"
+    p ">"
+    until valid_guess?(@guess)
+      @guess = gets.chomp
   end
 end
