8a0d79360add000185ef1c26e536a3944794e2e7
Jon Patterson <JonPatt92@gmail.com>
2019-10-18 15:37:44 -0600

    Add make_guess, restructure game class

--- lib/game.rb
+++ lib/game.rb
@@ -34,16 +34,35 @@ class Game
     end
   end
 
-  def valid_guess?
-
+  def valid_guess?(guess)
+    guess.join.count("RGBY") == 4
   end
 
-  def play_game
+  def make_guess
     p "I have generated a beginner sequence with four elements made up of: (r)ed,
     (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game.
     What's your guess?"
     p ">"
     until valid_guess?(@guess)
-      @guess = gets.chomp
+      @guess = gets.chomp.upcase.chars
+      if @guess.join.count("RGBY") > 4
+        p "That's too many letters. Guess again."
+        p ">"
+        @guess = gets.chomp.upcase.chars
+      elsif @guess.join.count("RGBY") < 4
+        p "That's too few letters. Guess again."
+        p ">"
+        @guess = gets.chomp.upcase.chars
+      end
+    end
+  end
+
+  def play_game
+    until @sequence.correct_guess?(@guess)
+      make_guess
+      @sequence.evaluate_guess(@guess)
+    end
+      p "Congratulations! You guessed the sequence 'GRRB' in 8 guesses over 4 minutes, 22 seconds."
+      p "Do you want to (p)lay again or (q)uit?"
   end
 end
